5a96fdff4dd27871054e69ab8ef013e91d4e0e9c
Nitishkumar <nitishkumar.blog@gmail.com>
2024-10-15 11:00:38 +0530

    Added JWT Auth

--- OnionTroveDB.sql
+++ OnionTroveDB.sql
@@ -1,4 +1,20 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+);
 CREATE TABLE TorMetricsMetaData (
     key TEXT PRIMARY KEY,
     value TIMESTAMP
+);
+
+CREATE TABLE TorMetrics (
+    date DATE NOT NULL UNIQUE,
+    TorRelayUsers INTEGER,
+    TorBridgeUsers INTEGER,
+    OnionSites INTEGER,
+    OnionServiceBandwidth FLOAT,
+    TorNetworkAdvertisedBandwidth FLOAT,
+    TorNetworkConsumedBandwidth FLOAT
 );
--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import os
 import json
 import psycopg2
 from pymongo import MongoClient
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, jsonify, session, redirect, url_for, flash
 from flask_compress import Compress
 from psycopg2.extras import RealDictCursor
+from functools import wraps
+import jwt
+from datetime import datetime, timedelta
 
 load_dotenv()
 
@@ -18,7 +21,64 @@ client = MongoClient(MongoDB)
 db = client['oniontrovedb'] 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SecretKey')
 Compress(app) 
+
+def IsLoggedIn(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if session:
+            token = session['SessionKey']
+        else:
+            flash('Please log in to access the page.', 'error')
+            return redirect(url_for('login'))
+        
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+        except jwt.ExpiredSignatureError:
+            flash('Token has expired!', 'error')
+            return redirect(url_for('login'))
+        except jwt.InvalidTokenError:
+            flash('Token is invalid.', 'error')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['login']
+        password = request.form['password']
+        with psycopg2.connect(**Postgres) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT id, password FROM users WHERE email = %s", (email,))
+                user = cur.fetchone()
+                if user:
+                    user_id, db_password = user
+                    if db_password == password:
+                        token = jwt.encode({
+                            'user_id': user_id,
+                            'exp': datetime.utcnow() + timedelta(hours=6)
+                        }, app.config['SECRET_KEY'], algorithm="HS256")
+                        session['SessionKey'] = token
+                        return redirect(url_for('dashboard'))
+                    else:
+                        flash('Invalid credentials.', 'error')
+                        return redirect(url_for('login'))
+                else:
+                    flash('User not found.', 'error')
+                    return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+@IsLoggedIn
+def dashboard():
+    return "Hi"
 
 @app.route('/map')
 def map():
